745fc596a5fe1e916e5022520c2b5db1c8a643ae
elkingutierrex <elkin.gutierrex@gmail.com>
2019-08-09 16:39:33 -0500

    102 - Página del artista, nueva ruta, parametro por url y servicio

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { SearchComponent } from './components/search/search.component';
+import { ArtistaComponent } from './components/artista/artista.component';
 
 export const ROUTES : Routes = [
   { path: 'home', component: HomeComponent},
   { path: 'search', component: SearchComponent},
-
+  { path: 'artist/:id', component: ArtistaComponent},
   { path: '', component: HomeComponent},
   { path: '**', component: HomeComponent},
 
